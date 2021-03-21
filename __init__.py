3b78699c60fd45f2bb3d504b884ede53fdfe3c8a
MURATAGAWA Kei <muratagawa@gmail.com>
2021-03-21 19:26:33 +0900

    Refine find_dimension_markers()

--- __init__.py
+++ __init__.py
@@ -28,34 +28,39 @@ bl_info = {
     "tracker_url": "https://github.com/muratagawa/dimensions-per-keyframe/issues",
 }
 
+MARKER_PATTERN = r"(\d+):(\d+)"
+
 
 class DPK_OT_save(Operator):
     bl_idname = "object.dpk_save"
-    bl_label = "Save Render Resolusion"
+    bl_label = "Save"
     bl_description = "Save dimensions"
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        # TODO
+        # TODO Overwrite if already exists
+
+        self.report({'INFO'}, "Dimensions saved.")
         return {'FINISHED'}
 
 
 class DPK_OT_delete(Operator):
     bl_idname = "object.dpk_delete"
-    bl_label = "Delete Render Resolusion"
+    bl_label = "Delete"
     bl_description = "Delete dimensions"
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
         # TODO
+        self.report({'INFO'}, "Dimensions deleted.")
         return {'FINISHED'}
 
 
 class DPK_PT_save_panel(Panel):
     bl_idname = "DPK_PT_save_panel"
-    bl_label = "Save/Delete to Keyframe Marker"
+    bl_label = "Save/Delete Dimensions to Keyframe Marker"
     bl_parent_id = "RENDER_PT_dimensions"
-    bl_context = "context"
+    # bl_context = "context"
     bl_space_type = "PROPERTIES"
     bl_region_type = "WINDOW"
 
@@ -65,28 +70,40 @@ class DPK_PT_save_panel(Panel):
         layout.operator(DPK_OT_delete.bl_idname)
 
 
-def find_dimensions():
+def find_dimension_markers():
+    result = []
+
     # Get markers of current frame
-    marker_items = bpy.context.scene.timeline_markers.items()
+    all_marker_items = bpy.context.scene.timeline_markers.items()
     current_frame = bpy.context.scene.frame_current
-    markers = [item[0]
-               for item in marker_items if item[1].frame == current_frame]
+    items = [item
+             for item in all_marker_items if item[1].frame == current_frame]
+
+    if len(items) < 1:
+        return result
+
+    # Parse marker strings to X,Y dimensions
+    for item in items:
+        matched = re.match(MARKER_PATTERN, item[0])
+        if matched:
+            result.append(item)
+
+    return result
+
+
+def get_dimensions_from_marker():
+    markers = find_dimension_markers()
 
     if len(markers) < 1:
         return "", ""
 
-    # Parse marker strings to X,Y dimensions
-    pattern = r"(\d+):(\d+)"
-    for m in markers:
-        matched = re.match(pattern, m)
-        if matched:
-            return int(matched.group(1)), int(matched.group(2))
-
-    return "", ""
+    marker_str = markers[0][0]
+    m = re.match(MARKER_PATTERN, marker_str)
+    return int(m.group(1)), int(m.group(2))
 
 
-def update_resolution(scene):
-    x, y = find_dimensions()
+def update_dimensions(scene):
+    x, y = get_dimensions_from_marker()
     if x != "":
         scene.render.resolution_x = x
         scene.render.resolution_y = y
@@ -103,14 +120,14 @@ def register():
     for c in classes:
         bpy.utils.register_class(c)
 
-    bpy.app.handlers.frame_change_pre.append(update_resolution)
+    bpy.app.handlers.frame_change_pre.append(update_dimensions)
 
 
 def unregister():
     for c in classes:
         bpy.utils.unregister_class(c)
 
-    bpy.app.handlers.frame_change_pre.remove(update_resolution)
+    bpy.app.handlers.frame_change_pre.remove(update_dimensions)
 
 
 if __name__ == "__main__":
